9c78ec8737302fe7b2f059aa47a991aed4f915c3
EdidiongEkpoh <euekpoh@gmail.com>
2023-05-24 23:30:12 +0000

    Made plots using ggplot2

--- Intro-To-GGPLOT.R
+++ Intro-To-GGPLOT.R
@@ -2,3 +2,48 @@
 # Eddie Ekpoh
 # 2023-05-23
 # edidionguekpoh@arizona.edu
+
+# (Ctrl + Shift + R)
+# Libraries ---------------------------------------------------------------
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("gapminder")
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(gapminder)
+#library(emoGG)
+
+
+# Making visualizations --------------------------------------------------
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(alpha = 0.5, col = "lightblue") +
+  #geom_emoji(emoji = "")
+  scale_x_log10() + 
+  geom_smooth(method = "lm", linewidth = 3, col = "darkblue") 
+
+ggplot(gapminder, aes(x = year, y = lifeExp, col = continent)) + 
+  geom_point() 
+
+# Categorical (year)
+ggplot(gapminder, aes(x = year, y = lifeExp,  group = country)) + 
+  geom_point() +
+  geom_line(aes(color = continent)) 
+
+gapminder <- gapminder
+
+# Visualize Americas only
+# unique(gapminder$continent)
+americas <- gapminder %>% #ctrl + shift + m 
+  filter(continent == "Americas")
+
+# Plot Americas
+ggplot(americas, aes(x = year, y = lifeExp, group = country)) +
+  geom_line(col = "blue") + 
+  facet_wrap(~country) + 
+  labs(x = "Year",
+       y = "Life Expectancy",
+       title = "Life Expectancy By Year")
+
+
